a52c466400f79ae2c277bbfca82831da6cd8cf48
Biplob Chakma <63535926+biplob-c@users.noreply.github.com>
2025-01-15 18:30:12 +0600

    Create Lexical.c

---
+++ Lexical.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<stdbool.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+    char str[100];
+    printf("Toe the line of code: \n");
+    scanf("%[^\n]", str);
+    return 0;
+}
+
